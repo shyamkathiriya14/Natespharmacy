bfffbb76aabadf8c25626ab31ebeac0972c14790
shyam kathiriya <shyamkathiriya.14@gmail.com>
2025-12-29 22:50:54 +0530

    create splash screen animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
-import Home from './pages/Home/Home';
+import { useEffect, useState } from "react";
+import Home from "./pages/Home/Home";
+import SplashScreen from "./components/sections/SplashScreen/SplashScreen";
 
 function App() {
+  const [hideSplash, setHideSplash] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setHideSplash(true);
+    }, 2400);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-   <>
-     <Home/>
-   </>
+    <>
+      <Home />
+      {!hideSplash && <SplashScreen/>}
+    </>
   );
 }
 
