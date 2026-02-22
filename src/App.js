cc60b7890341406bcc1e4b664c9cda69818805e2
shyam kathiriya <shyamkathiriya.14@gmail.com>
2026-02-22 17:48:58 +0530

    remove job page

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     <>
       <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/careers" element={<Careers />} />
+      {/* <Route path="/careers" element={<Careers />} /> */}
     </Routes>
       {!hideSplash && <SplashScreen/>}
     </>
